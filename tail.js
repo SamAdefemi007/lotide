aa3fc2af5c4ccbe78124a545482b57772a33b3f3
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2023-04-03 18:31:16 -0600

    added tail.js

---
+++ tail.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (arr) => {
+  if (arr.length < 2) {
+    return [];
+  }
+  return arr.slice(1);
+};
+const words = ["Hello", "Lighthouse", "Labs"];
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
+assertEqual(words.length, 3);
